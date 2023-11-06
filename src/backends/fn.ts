4b22fd6f4ef3595e0537ebb26161ea05bf62a5b6
Negezor <negezor@gmail.com>
2023-11-06 14:58:59 +1100

    optimize: skip missing replacements in template

--- src/backends/fn.ts
+++ src/backends/fn.ts
@@ -9,14 +9,18 @@ export interface CompileFnBackendOptions<K extends string> {
 export function compileFnBackend<K extends string>(template: string, options: CompileFnBackendOptions<K>): Render<K> {
     const placeholderEntries = Object.entries(options.placeholders) as [K, string][];
 
+    const existingPlaceholder = placeholderEntries.filter(([, search]) => (
+        template.includes(search)
+    ));
+
     // OPTIMIZE: If you don't need to change anything, just return the string
-    if (placeholderEntries.length === 0) {
+    if (existingPlaceholder.length === 0) {
         return () => template;
     }
 
     let newTemplate = `\`${template.replace(TEMPLATE_QUOTE_RE, '\\`')}\``;
 
-    for (const [key, value] of placeholderEntries) {
+    for (const [key, value] of existingPlaceholder) {
         newTemplate = newTemplate.replaceAll(value, `\${data.${key}}`);
     }
 
